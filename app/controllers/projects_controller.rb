12494f41736bc9419855503bd917ea66c3d4274d
Joseph Lozano <lozanojoseph@gmail.com>
2015-10-05 15:11:52 -0700

    projects controller index  --skip-ci

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,10 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = Project.all
-    render json: @projects
+    render json: @projects, methods: [:difficulty_name, :owner]
+  end
+
+  def create
+
   end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+  before do
+    create :membership
+    get :index
+  end
 
+  it 'should have a response' do
+    expect(response.body).to_not be(nil)
+  end
+
+  it 'should have a project title' do
+    expect(JSON.parse(response.body).first['title']).to eql('myProject')
+  end
+
+  it 'should have a difficulty name' do
+    expect(JSON.parse(response.body).first['difficulty_name']).to eql('Beginner')
+  end
+
+  it 'should have a project_owner' do
+    expect(JSON.parse(response.body).first['owner']['email']).to include('myemail@user', ".com")
+  end
 end
