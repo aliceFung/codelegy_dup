3d0df9a355fd0ebade9ec271060e21afcc25e8d6
facingsouth <xin.d.liu@gmail.com>
2015-10-05 13:50:06 -0700

    test callback controller  --skip-ci

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,30 +1,36 @@
 require 'rails_helper'
 
+include Devise::TestHelpers
+
 describe Users::SessionsController do
 
-  # let(:user) { create(:user) }
+  # let!(:user) { create(:user) }
+  # let(:json) { JSON.parse(response.body) }
 
   # context 'RESTful actions' do
 
   #   describe 'POST #create' do
 
   #     specify 'create a session on sign in' do
-  #       binding.pry
-  #       expect do
-  #         post :create, user: attributes_for(:user)
-  #       end
+  #       # request.env["devise.mapping"] = Devise.mappings[:user]
+  #       # # binding.pry
+  #       # post :create#, user: :user, format: :json
+  #       sign_in :user, user
 
+  #       binding.pry
+  #       expect(json.username).to eq(user.username)
+  #       expect(json.email).to eq(user.email)
+  #       expect(json.provider).to eq(user.provider)
+  #       expect(json.uid).to eq(user.uid)
   #     end
 
-  #     specify 'create a session on sign up'
-
   #   end
 
-  #   describe 'DELETE #destroy' do
+    # describe 'DELETE #destroy' do
 
-  #     specify 'destroy a session on sign out'
+    #   specify 'destroy a session on sign out'
 
-  #   end
+    # end
 
   # end
 
