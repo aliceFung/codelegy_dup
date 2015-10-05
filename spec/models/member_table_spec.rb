a7d6219cdb9f7026f9a61f8995f536f4b603d785
Joseph Lozano <lozanojoseph@gmail.com>
2015-10-05 09:31:49 -0700

    member_table tests  --skip-ci

--- spec/models/member_table_spec.rb
+++ spec/models/member_table_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe MemberTable, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:table) { create :member_table }
+
+  it 'should have a project' do
+    expect(table.project).to be_a(Project)
+  end
+
+  it 'should have a user' do
+    expect(table.user).to be_a(User)
+  end
 end
