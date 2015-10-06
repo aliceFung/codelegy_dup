71aac425f21740c84bc6b1574501492af0dec38b
shadefinale <shadefinale@gmail.com>
2015-10-06 15:10:49 -0500

    Fix dependancy injection. --skip-ci

--- app/assets/javascripts/controllers/FormController.js
+++ app/assets/javascripts/controllers/FormController.js
@@ -1,11 +1,11 @@
-app.controller('formController', function($scope) {
-    
+app.controller('formController', ['$scope', function($scope) {
+
     // we will store all of our form data in this object
     $scope.formData = {};
-    
+
     // function to process the form
     $scope.processForm = function() {
-        alert('awesome!');  
+        alert('awesome!');
     };
-    
-});
+
+}]);
