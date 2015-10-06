15575368e99c34728d3e8ec79005c1800b605263
Joseph Lozano <lozanojoseph@gmail.com>
2015-10-06 16:13:28 -0700

    tests for language and proejcts  --skip-ci

--- spec/controllers/languages_controller_spec.rb
+++ spec/controllers/languages_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe LanguagesController, type: :controller do
+  let(:user){ create :user }
+  it 'serves languages' do
+    create :language
+    sign_in(user)
+    get :index
+    expect(JSON.parse(response.body).length).to eql(Language.count)
+  end
 
+  it 'serves language name' do
+    create :language
+    sign_in(user)
+    get :index
+    expect(JSON.parse(response.body).first['url']).to eql('/logos/lang.png')
+  end
+
+  it 'serves language url' do
+    create :language
+    sign_in(user)
+    get :index
+    expect(JSON.parse(response.body).first['name']).to eql('MyString')
+  end
 end
