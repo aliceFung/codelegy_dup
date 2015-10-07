1358025a227c619b0e77b136941f49553890bb8a
facingsouth <xin.d.liu@gmail.com>
2015-10-06 17:00:09 -0700

    work on font end validation for sign up form  --skip-ci

--- app/assets/javascripts/controllers/profileRegistrationCtrl.js
+++ app/assets/javascripts/controllers/profileRegistrationCtrl.js
@@ -1,10 +1,14 @@
 app.controller('profileRegistrationCtrl',
   ['$scope',
-   'profileRegistration',
-    function($scope, profileRegistration){
+   'profileRegistration', 
+   '$state', 
+    function($scope, profileRegistration, $state){
       $scope.profileInput = profileRegistration.profileInput;
       $scope.languages = profileRegistration.languages;
       $scope.expLevel = profileRegistration.expLevel;
+      $scope.signupForm = {};
+
+      $scope.emailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
 
       $scope.setExpLevel = function(languageId, levelId) {
         console.log(languageId, levelId);
@@ -13,6 +17,13 @@ app.controller('profileRegistrationCtrl',
           delete $scope.profileInput.profile_languages[languageId];
         } else {
           $scope.profileInput.profile_languages[languageId] = levelId;
+        }
+      };
+
+      $scope.processForm = function(formIsValid){
+        // alert('form valid: ', formIsValid);
+        if(formIsValid){
+          $state.go('home.form.languages');
         }
       };
 }]);
