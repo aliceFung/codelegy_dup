62ddec64fb43ed14c69816059c787555e6279417
Joseph Lozano <lozanojoseph@gmail.com>
2015-10-08 08:42:28 -0700

    add associations and migration columns  --skip-ci

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007152031) do
+ActiveRecord::Schema.define(version: 20151008153840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,14 @@ ActiveRecord::Schema.define(version: 20151007152031) do
     t.string   "availability"
     t.string   "description"
     t.integer  "difficulty_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "suggestions", force: :cascade do |t|
+    t.integer  "language_id",   null: false
+    t.integer  "difficulty_id"
+    t.text     "description"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
