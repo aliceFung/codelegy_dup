5301625ccf7910ce1319981ff96b07d2c4ed379a
shadefinale <shadefinale@gmail.com>
2015-10-08 11:29:55 -0500

    Send messages when a user is accepted/rejected.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :user do
     sequence(:username) { |n| "foo#{n}" }
-    sequence(:email) {|n| "myemail@user#{n}.com"}
+    sequence(:email) {|n| "myemail@user#{n*120}.com"}
     # sequence(:email) { |n| "foo#{n}@bar.com"}
     password '12345678'
     password_confirmation '12345678'
