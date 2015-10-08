18de052e38554cb66f159b4d7f19c380e773065a
shadefinale <shadefinale@gmail.com>
2015-10-08 16:45:04 -0500

    Change some config settings for capistrano deploy. --skip-ci

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Codelegy
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.active_job.queue_adapter = :delayed_job
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
