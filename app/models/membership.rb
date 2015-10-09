333da2bdc29e8541e14f1f154701be45ba0bd417
shadefinale <shadefinale@gmail.com>
2015-10-09 10:29:57 -0500

    Fix delayed job mails #1. --skip-ci

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -8,12 +8,12 @@ class Membership < ActiveRecord::Base
   validates :project, presence: true
 
   def send_delayed_request_email
-    Membership.send_request_email(self.user_id, self.project_id)
+    Membership.delay.send_request_email(self.user_id, self.project_id)
   end
 
   def self.send_request_email(user_id, project_id)
-    user = User.find_by_id(user_id)
-    project = Project.find_by_id(project_id)
+    user = User.find(user_id)
+    project = Project.find(project_id)
     UserMailer.request_membership(user, project).deliver_now! if user && project
   end
 end
