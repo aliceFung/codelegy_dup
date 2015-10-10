4fc832ab7f0ec48ef9c1eead4fdd32d3a17c60d8
alokp <alokpra@gmail.com>
2015-10-10 13:26:04 -0700

    edit languages from user dashboard complete  --skip-ci

--- app/assets/javascripts/controllers/dashboardProfileCtrl.js
+++ app/assets/javascripts/controllers/dashboardProfileCtrl.js
@@ -12,41 +12,26 @@ app.controller('dashboardProfileCtrl',
       $scope.expLevel = ProfileRegistration.expLevel;
 
       $scope.profileUpdates = {};
+      $scope.profileUpdates.profile_languages = {};
       // $scope.availability = '';
       // $scope.about = '';
       // $scope.languages = '';
 
       $scope.updatedDifficulty = {};  // language_id : difficulty_id
-      // $scope.profileUpdates.profile_languages = $scope.languages;
 
-      $scope.changeDifficulty = function(language_id, difficulty, difficulty_id){
+      $scope.changeDifficulty = function(language_id, difficulty, difficulty_id, pl_id){
         $scope.updatedDifficulty[language_id] = difficulty_id;
-      };
 
-      $scope.updateLanguage = function(){
-        for (var i = 0; i < $scope.languages.length; i++){
-          var current_language_id = $scope.languages[i].id.toString();
-          console.log('current_language_id', current_language_id);
-
-          var languagesIdsUpdated = Object.keys($scope.updatedDifficulty);
-          var index = languagesIdsUpdated.indexOf(current_language_id);
-
-          console.log('keys', Object.keys($scope.updatedDifficulty));
-          console.log(index);
-
-          if (index > -1){
-            if ($scope.updatedDifficulty[current_language_id] == 'None') {
-              delete $scope.profileUpdates.profile_languages[i];
-            } else {
-              $scope.profileUpdates.profile_languages[i] = $scope.updatedDifficulty[current_language_id];
-            }
-          }
+        if (difficulty == 'None') {
+          delete $scope.profileUpdates.profile_languages[language_id];
+        } else {
+          $scope.profileUpdates.profile_languages[language_id] = [];
+          $scope.profileUpdates.profile_languages[language_id][0] = difficulty_id;
+          $scope.profileUpdates.profile_languages[language_id][1] = pl_id;
         }
-        $scope.update();
       };
 
       $scope.update = function(){
-        console.log($scope.profileUpdates);
         Auth.currentUser().then(function(user){
           ProfileRegistration.update(user.id, $scope.profileUpdates, $scope.information);
         });
--- app/assets/javascripts/services/profileRegistrationService.js
+++ app/assets/javascripts/services/profileRegistrationService.js
@@ -5,7 +5,7 @@ app.factory('ProfileRegistration',
     1: 'Beginner',
     2: 'Intermediate',
     3: 'Advanced',
-    4: 'Expert',
+    4: 'Expert'
   };
 
   var profileInput = { profile_languages: {} }; //{ languages: { name: '', expLevel: '' } };
@@ -14,26 +14,34 @@ app.factory('ProfileRegistration',
     var profileInfo = {};
     // Send information to backend to update user profile
     profileInfo.user_id = user_id;
-    // console.log(profileInfo.user_id);
     profileInfo.availability = profile.availability;
     profileInfo.about = profile.about;
     profileInfo.profile_languages_attributes = processProfileInput(profile.profile_languages);
 
-    Restangular.all('profiles').customPUT({profile: profileInfo}).then(function(profile){
-      // console.log('updated profile: ', profile);
-      information.about = profile.about;
-      information.availability = profile.availability;
-      information.profile_languages_attributes = profile.profile_languages_attributes;
+    Restangular.all('profiles').customPUT({profile: profileInfo}).then(function(profile_returned){
+      information.about = profile_returned.about;
+      information.availability = profile_returned.availability;
+      updateProfileLanguagesDisplayed(information.profile_languages, profile_returned.profile_languages);
     }, function(error) {
-      // console.log('cannot update profile');
+      alert('cannot update profile', error.data);
     });
+  }
+
+  function updateProfileLanguagesDisplayed(front_end_information, returned_profile_attr){
+    for (var key in front_end_information) {
+      delete front_end_information[key];
+    }
+
+    for(var newkey in returned_profile_attr){
+      front_end_information[newkey] = returned_profile_attr[newkey];
+    }
   }
 
   function processProfileInput(profileInput) {
     var count = 1;
     var result = {};
     for(var key in profileInput) {
-      result[count] = {language_id: key, difficulty_id: profileInput[key]};
+      result[count] = {language_id: key, difficulty_id: profileInput[key][0], id: profileInput[key][1]};
       count++;
     }
     return result;
