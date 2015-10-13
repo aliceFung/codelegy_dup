bbdd669cef09ddfed2d72a1f875c53bc8c957571
facingsouth <xin.d.liu@gmail.com>
2015-10-13 15:46:24 -0700

    day_timeslot rspec  --skip-ci

--- spec/models/day_timeslot_spec.rb
+++ spec/models/day_timeslot_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe DayTimeslot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:day_timeslot) { create(:profile_timeslot) }
+
+  context 'Accociation' do
+    it 'should respond to day method' do
+      expect(day_timeslot).to respond_to(:day)
+    end
+
+    it 'should respond to timeslot method' do
+      expect(day_timeslot).to respond_to(:timeslot)
+    end
+
+    it 'should respond to owner method' do
+      expect(day_timeslot).to respond_to(:owner)
+    end
+  end
+
+  # context 'Validation' do
+  #   it 'should raise error when create a new day_timeslot with identical (day, timeslot, owner) combination' do
+  #     new_day_timeslot = build(:profile_timeslot)
+  #     new_day_timeslot.day_id = day_timeslot.day_id
+  #     new_day_timeslot.timeslot_id = day_timeslot.timeslot_id
+  #     new_day_timeslot.owner_id = day_timeslot.owner_id
+  #     new_day_timeslot.owner_type = day_timeslot.owner_type
+  #     binding.pry
+  #     expect(new_day_timeslot.save).not_to raise_error(ActiveRecord::RecordInvalid)
+  #   end
+  # end
 end
