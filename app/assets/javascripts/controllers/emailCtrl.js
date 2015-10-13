7998d06bf5885502af1ef452efeb4df03fb21400
Alice Fung <alice.o.fung@gmail.com>
2015-10-13 12:04:17 -0400

    removed emailService from sessionCtrl to keep it from prematurely querying data without signin  --skip-ci

--- app/assets/javascripts/controllers/emailCtrl.js
+++ app/assets/javascripts/controllers/emailCtrl.js
@@ -1,7 +1,7 @@
 app.controller('emailCtrl', ['$scope', 'emailService', 'Restangular',
   function($scope, emailService, Restangular) {
 
-  // console.log('emailCtrl initiated');
+  console.log('emailCtrl initiated');
 
   $scope.inbox = emailService.inbox;
 
--- app/assets/javascripts/services/emailService.js
+++ app/assets/javascripts/services/emailService.js
@@ -1,10 +1,13 @@
 app.factory('emailService', ['Restangular', function(Restangular){
 
+  console.log('emailService started');
+
   var inbox = [];
   //get emails for inbox
   Restangular.all('mailbox').getList().then(
     function(result){
       inbox.push.apply(inbox, result);
+      // console.log('results', result);
     }
   );
 
